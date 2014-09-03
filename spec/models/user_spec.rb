2be0b68cec6433b970e2fff7559b1f045e9217e4
Michael Hall <mehall1@email.neit.edu>
2014-09-02 17:22:53 -0700

    thanks to clark!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+    password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -13,16 +14,17 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
-  #it { should be_valid }
+  it { should be_valid }
 
   describe "when name is not present" do
-    before { @user.name = " " }
+    before { @user.name = "" }
     it { should_not be_valid }
   end
 
   describe "when email is not present" do
-    before { @user.email = " " }
+    before { @user.email = "" }
     it { should_not be_valid }
   end
 
@@ -34,7 +36,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -47,7 +49,7 @@ describe User do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
-        #expect(@user).to be_valid
+        expect(@user).to be_valid
       end
     end
   end
@@ -65,7 +67,7 @@ describe User do
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
+      password: "", password_confirmation: "")
     end
     it { should_not be_valid }
   end
@@ -74,8 +76,6 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-
-  it { should respond_to(:authenticate) }
 
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
@@ -97,8 +97,6 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
-
-
 end
 
 
